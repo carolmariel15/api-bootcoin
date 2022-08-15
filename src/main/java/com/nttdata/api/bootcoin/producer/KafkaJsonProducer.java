10ba0d601c46e05d930339ba27cec5ee272054ea
Carol Ñavincopa <cnavinco@nttdata.com>
2022-08-15 11:24:52 -0500

    agrego comentario prueba

--- src/main/java/com/nttdata/api/bootcoin/producer/KafkaJsonProducer.java
+++ src/main/java/com/nttdata/api/bootcoin/producer/KafkaJsonProducer.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+/*pruebaaaaaaa*/
+
 @Component
 public class KafkaJsonProducer {
 
